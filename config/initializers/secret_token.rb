75b45a63c8f54418d302ec17241b33de76bc9d18
Jorge Moscat <jorgemoscat@gmail.com>
2013-03-14 11:25:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfluenceWeb::Application.config.secret_token = '71a7222f65be0a37093725cdfca9e0614df8dbcfaf1315d2d8ab7cb51ef530a6707f8d89a6e78e2af6dfcd772fcb8f5b2066dc1d6a83b5e168e371d0d4743b06'
