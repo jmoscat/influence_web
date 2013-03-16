4b5de60a60b0e030b7ecd7b861879f4de89b1a1f
Jorge Moscat <jorgemoscat@gmail.com>
2013-03-15 18:50:30 -0700

    CA issue with heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '337447679708646', '6a2e9afc580b2a35c17e500b6a0c6a77',
-  :scope => 'email,user_hometown,user_likes,user_location,user_status,read_stream,status_update'
+  {:scope => 'email,user_hometown,user_likes,user_location,user_status,read_stream,status_update',
+  	:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
